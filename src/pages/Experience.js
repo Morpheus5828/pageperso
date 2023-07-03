40b53098899b3cd559c0bc3340168bc6ac0f4598
Marius THORRE <thorre.m@yahoo.com>
2023-07-03 14:34:11 +0200

    add rel=noopener noreferrer in Experience.js

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -5,9 +5,9 @@ const Experience = () => {
     return (
         <div className={"div_experience"}>
             <div className={"internship"}>
-                <h4>Internship at <a href={"https://qarma.lis-lab.fr/"} target={"_blank"}>QARMA - LIS Team</a> (2023)</h4>
-                <h4>Internship at <a href={"https://talep.lis-lab.fr/"} target={"_blank"}>TALEP - LIS Team</a> (2022)</h4>
-                <h4>Internship at <a href={"https://coala.lis-lab.fr/"} target={"_blank"}>COALA - LIS Team</a> (2021)</h4>
+                <h4>Internship at <a href={"https://qarma.lis-lab.fr/"} rel="noopener noreferrer" target={"_blank"}>QARMA - LIS Team</a> (2023)</h4>
+                <h4>Internship at <a href={"https://talep.lis-lab.fr/"} rel="noopener noreferrer" target={"_blank"}>TALEP - LIS Team</a> (2022)</h4>
+                <h4>Internship at <a href={"https://coala.lis-lab.fr/"} rel="noopener noreferrer" target={"_blank"}>COALA - LIS Team</a> (2021)</h4>
             </div>
             <div className={"employment"}>
                 <h4>AED TICE at COLBERT High School in Marseille, France (April 2022 - September 2023)</h4>
